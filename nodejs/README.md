2d69395c75bad3ce39cae4d74fd8a638dd2ac61a
igorfachini <igor.fachini@catolicasc.org.br>
2018-01-25 23:03:31 -0200

    up init repo

---
+++ nodejs/README.md
@@ -0,0 +1,17 @@
+anotacoes - 10:00
+
+(nodejs)
+
+BANCO funcao=
+//Diferente das outras linguagens, pra consulta, nessecita passar uma funcao como resultado apos pesquisa.
+
+## //Midware - funcoes que executam antes da requisicao chegar.
+
+libys
+('express-load');// Carrega modulos automaticamente
+
+(body-parser) - pegar a requisicao que vai entrar no POST, e preenche a propriedade body do request.
+
+---
+
+(\_) antes do nome da funcao/variavel em js, indica que ela é privada(acessivel mas, pra apenas indicar pro DEV a regra).
---
+++ nodejs/casadocodigo/config/express.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var load = require('express-load');// Carrega modulos automaticamente
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+module.exports = function() {
+
+    var app = express();
+    app.use(express.static('./public'));
+    app.set('view engine', 'ejs');
+    app.set('views','./app/views');
+
+    // extended, formularios q tem interligacao com outros dados,
+    //  ex: (add bibliotecas -> livros) n sao entendidos mais, 
+    // extended abilita o entendimento
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(expressValidator());
+
+    // carrega rotas(procura em todas aplicacao(cwd limita o local de procura))
+    //  em app,depois carrega os de infra e deixa na var app, 
+    load('routes',{cwd: 'app',verbose:true})
+        .then('infra')
+        .into(app);
+
+    app.use(function(req, res, next){
+        res.status(404).render("erros/404");
+    });
+
+    app.use(function(error,req, res, next){
+        res.status(500).render("erros/500");
+    });
+    //tem que colocar na ordem, caso contrário ele passa pelo middleware e ainda não vai ter acontecido nenhum erro.
+
+
+
+
+    return app;
+};
