cce30a8a5447316783d0677cb3d92a8ac1e3fec9
igorfachini <igor.fachini@catolicasc.org.br>
2018-02-19 10:20:27 -0300

    pwa path temp fix

--- pwa/work/service-worker.js
+++ pwa/work/service-worker.js
@@ -1,23 +1,23 @@
 var dataCacheName = "weatherData-v1";
 var cacheName = "weatherPWA-step-6-3";
 var filesToCache = [
-  "/",
-  "/index.html",
-  "/scripts/app.js",
-  "/styles/inline.css",
-  "/images/clear.png",
-  "/images/cloudy-scattered-showers.png",
-  "/images/cloudy.png",
-  "/images/fog.png",
-  "/images/ic_add_white_24px.svg",
-  "/images/ic_refresh_white_24px.svg",
-  "/images/partly-cloudy.png",
-  "/images/rain.png",
-  "/images/scattered-showers.png",
-  "/images/sleet.png",
-  "/images/snow.png",
-  "/images/thunderstorm.png",
-  "/images/wind.png"
+  "/pwa/work/",
+  "/pwa/work/index.html",
+  "/pwa/work/scripts/app.js",
+  "/pwa/work/styles/inline.css",
+  "/pwa/work/images/clear.png",
+  "/pwa/work/images/cloudy-scattered-showers.png",
+  "/pwa/work/images/cloudy.png",
+  "/pwa/work/images/fog.png",
+  "/pwa/work/images/ic_add_white_24px.svg",
+  "/pwa/work/images/ic_refresh_white_24px.svg",
+  "/pwa/work/images/partly-cloudy.png",
+  "/pwa/work/images/rain.png",
+  "/pwa/work/images/scattered-showers.png",
+  "/pwa/work/images/sleet.png",
+  "/pwa/work/images/snow.png",
+  "/pwa/work/images/thunderstorm.png",
+  "/pwa/work/images/wind.png"
 ];
 
 self.addEventListener("install", function(e) {
