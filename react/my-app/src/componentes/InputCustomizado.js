cc3009862368bed7b6adf137110ca02ebe0de27a
igorfachini <igor.fachini@catolicasc.org.br>
2018-07-26 00:06:24 -0300

    REACT doc up

--- react/my-app/src/componentes/InputCustomizado.js
+++ react/my-app/src/componentes/InputCustomizado.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // JSX exige que nome do mudulo importa de react seja chamado de REACT.
 
+// deve ser exportado para ser enxergado.
 export default class InputCustomizado extends Component{
 
 	render() {
 		return (
 			<div className="pure-control-group">
+			//propriedades do component sao acessadas com prop herdado de Component
+
 			  <label htmlFor={this.props.id}>{this.props.label}</label> 
 			  <input id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value}  onChange={this.props.onChange}/>                  
 			</div>			
