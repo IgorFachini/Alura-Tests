717f25abe33a54844e9636e40e45746303920e61
igorfachini <igor.fachini@catolicasc.org.br>
2018-07-26 00:08:04 -0300

    REACT retrive doc inside render

--- react/my-app/src/componentes/InputCustomizado.js
+++ react/my-app/src/componentes/InputCustomizado.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react'; // JSX exige que nome do mudulo import
 
 // deve ser exportado para ser enxergado.
 export default class InputCustomizado extends Component{
-
+	//propriedades do component sao acessadas com prop herdado de Component
 	render() {
 		return (
 			<div className="pure-control-group">
-			//propriedades do component sao acessadas com prop herdado de Component
-
 			  <label htmlFor={this.props.id}>{this.props.label}</label> 
 			  <input id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value}  onChange={this.props.onChange}/>                  
 			</div>			
